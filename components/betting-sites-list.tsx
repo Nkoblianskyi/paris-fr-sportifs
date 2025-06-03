27a51e3bb488a4ff00a448002c25af62d6316583
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-06-03 11:01:58 +0200

    1

--- components/betting-sites-list.tsx
+++ components/betting-sites-list.tsx
@@ -8,10 +8,12 @@ export default function BettingSitesList() {
     <section className="py-12 relative">
       <div className="container mx-auto px-4 relative z-10">
         <FadeIn>
-          <h2 className="text-2xl md:text-3xl font-bold text-center mb-6 md:mb-8 pt-4 md:pt-0 text-white">
-            Top Sites de Paris Sportifs{" "}
-            {new Date().toLocaleDateString("fr-FR", { month: "2-digit", year: "numeric" }).replace("/", ".")}
-          </h2>
+          <div className="text-center">
+            <h2 className="text-2xl md:text-3xl font-bold text-center pt-4 md:pt-0 text-white">
+              Top Sites de Paris Sportifs{" "}
+            </h2>
+            <p className="text-white text-lg mb-2">{new Date().toLocaleDateString("fr-FR", { month: "2-digit", year: "numeric" }).replace("/", ".")}</p>
+          </div>
         </FadeIn>
         <StaggerContainer staggerDelay={150} className="space-y-6">
           {bettingSites.map((site, index) => (
